bd5596ee44ba3e639726be9d78c87dce1dd0ac8a
heeyunki-mp3 <82672640+heeyunki-mp3@users.noreply.github.com>
2023-04-19 16:19:30 -0400

    merge

--- src/serverSock.cpp
+++ src/serverSock.cpp
@@ -24,8 +24,8 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "ERROR, no port provided\n");
 		exit(1);
 	}
+    
 	// create a socket
-	// socket(int domain, int type, int protocol)
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sockfd < 0)
 		error("ERROR opening socket");
@@ -45,15 +45,9 @@ int main(int argc, char *argv[])
 	// convert short integer value for port must be converted into network byte order
 	serv_addr.sin_port = htons(portno);
 
-	// bind(int fd, struct sockaddr *local_addr, socklen_t addr_length)
-	// This bind() call will bind  the socket to the current IP address on port, portno
 	if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
 		error("ERROR on binding");
 
-	// This listen() call tells the socket to listen to the incoming connections.
-	// The listen() function places all incoming connection into a backlog queue
-	// until accept() call accepts the connection.
-	// Here, we set the maximum size for the backlog queue to 5.
     while(true){
         listen(sockfd, 5);
         // The accept() call actually accepts an incoming connection
