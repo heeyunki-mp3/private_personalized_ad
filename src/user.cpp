2e5b269234a69153b8b9c33915bb866a7e462dbb
Ryan Wee <rw2800@columbia.edu>
2023-05-10 14:41:22 -0400

    Chore: Remove loggin

--- src/user.cpp
+++ src/user.cpp
@@ -208,16 +208,16 @@ void UserProgram::doEncryptionSetup() {
 
 	while (n == 2047) {
 		paramString.append(buffer);
-		std::cout << "buffer: " << paramString << endl;
+		// std::cout << "buffer: " << paramString << endl;
 		hexDump(buffer, 2047);
 		n = read(socketfd_, buffer, 2047);
 	}
 	paramString.append(buffer);
 
-	std::cout << "Socket: recieved param" << endl;
+	// std::cout << "Socket: recieved param" << endl;
 
-	std::cout << "Main: Initializing pir client"
-		  << "Enc size: " << sizeof(EncryptionParameters) << " pir size: " << sizeof(PirParams) << endl;
+	// std::cout << "Main: Initializing pir client"
+	// 	  << "Enc size: " << sizeof(EncryptionParameters) << " pir size: " << sizeof(PirParams) << endl;
 
 	// copying the received information to local buffers
 	unsigned long enc_size;
@@ -227,26 +227,26 @@ void UserProgram::doEncryptionSetup() {
 	::memcpy(pir_param_buffer, buffer +sizeof(unsigned long)+enc_size, sizeof(PirParams));
 	::memcpy(&pir_nvec_len, buffer +sizeof(unsigned long)+enc_size + sizeof(PirParams), sizeof(unsigned long));
 	pir_vector_buffer = malloc(pir_nvec_len);
-	printf("pir allocated at: %p with %ld size\n", pir_vector_buffer, pir_nvec_len);
+	// printf("pir allocated at: %p with %ld size\n", pir_vector_buffer, pir_nvec_len);
 	::memcpy(pir_vector_buffer, buffer +sizeof(unsigned long)+enc_size + sizeof(PirParams) + sizeof(unsigned long), pir_nvec_len);
 
-	std::cout << "Main: copied params" << endl;
-	std::cout << "vector buffer:" << endl;
+	// std::cout << "Main: copied params" << endl;
+	// std::cout << "vector buffer:" << endl;
 
 	// turning the buffered bits into objects
 	// deserialzing pir object
 	PirParams pir_param_object;
 	memcpy(&pir_param_object, pir_param_buffer, sizeof(PirParams));
-	std::cout << "Main: copied pir param" << endl;
+	// std::cout << "Main: copied pir param" << endl;
 
 	// deserializing pir.nvec
 	std::vector<std::uint64_t> nvec_hold;
 
 	deserialize_vector(nvec_hold, (char *)pir_vector_buffer);
-	std::cout << "Main: deserialized vector" << endl;
+	// std::cout << "Main: deserialized vector" << endl;
 	pir_param_object.nvec = nvec_hold;
 
-	my_print_pir_params(pir_param_object);
+	// my_print_pir_params(pir_param_object);
 
 	EncryptionParameters enc_param_object;
 	std::stringstream enc_stream;
@@ -257,30 +257,30 @@ void UserProgram::doEncryptionSetup() {
 	PIRClient client(enc_param_object, pir_param_object);
     pirclient_ = &client;
 
-	std::cout << "Main: created galois key" << endl;
+	// std::cout << "Main: created galois key" << endl;
 	GaloisKeys galois_keys = client.generate_galois_keys();
 	bzero(buffer, 2048);
 
-	std::cout << "Socket: sending galois key" << endl;
+	// std::cout << "Socket: sending galois key" << endl;
 	std::string galois_str = serialize_galoiskeys(galois_keys);
 	unsigned long gal_len = galois_str.size();
 
-	std::cout << "Socket: sending galois key len" << endl;
+	// std::cout << "Socket: sending galois key len" << endl;
 
 	::memcpy(buffer, &gal_len, sizeof(unsigned long));
 	
 	n = write(socketfd_, buffer, sizeof(unsigned long));
-	std::cout << "Socket: sent galois key len" << endl;
+	// std::cout << "Socket: sent galois key len" << endl;
 
 	n = read(socketfd_, buffer, 2047);
 	if (buffer[0] == 'A' && buffer[1] == 'C' && buffer[2] == 'K') {
-		std::cout << "Socket: received ACK for Galois key length" << endl;
+		// std::cout << "Socket: received ACK for Galois key length" << endl;
 	}
-	std::cout << "Socket: sending galois key of length "<< gal_len << endl;
+	// std::cout << "Socket: sending galois key of length "<< gal_len << endl;
 
 	n = write(socketfd_, galois_str.data(), gal_len);
 
-	std::cout <<"Socekt: done sending galois key"<<endl;
+	// std::cout <<"Socekt: done sending galois key"<<endl;
 
 	n = read(socketfd_, buffer, 2047);
 	if (buffer[0] == 'A' && buffer[1] == 'C' && buffer[2] == 'K') {
